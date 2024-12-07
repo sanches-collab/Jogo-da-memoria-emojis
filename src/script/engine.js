7a82511c3e00d7901b1daea9ff7a84753848ae90
sanches-collab <96600368+sanches-collab@users.noreply.github.com>
2024-12-06 22:27:59 -0300

    Update engine.js

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,53 +1,42 @@
-
-const emojis =[
-"😺",
-"😺",
-"🦝",
-"🦝",
-"🦊",
-"🦊",
-"🦋",
-"🦋",
-"🐶",
-"🐶",
-"🐵",
-"🐵",
-"🦁",
-"🦁",
-"🐨",
-"🐨",
+const emojis = [
+    "😺", "😺", "🦝", "🦝", "🦊", "🦊", "🦋", "🦋",
+    "🐶", "🐶", "🐵", "🐵", "🦁", "🦁", "🐨", "🐨"
 ];
-let openCards=[];
 
-let shuffleEmojis= emojis.sort(()=>(Math.random()>0.5 ? 2 : -1));
+let openCards = [];
 
-for(let i=0; i < emojis.length; i++){
-let box = document.creatElement("div");
-box.className= "item";
-box.innerHTML= shuffleEmojis[i];
-box.onclick= handleClick;
-document.queryselector(".game").appendChild(box);
-}
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-fuction handleClick(){
-if(openCards.length < 2){
-  this.classList.add("boxOpen");
-openCards.push(this);
-}
-if(openCards.length== 2){
- setTimeout(checkMatch, 500);
-}
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
 }
 
-function checkMatch(){
- if(openCards [0].INNER HTML === openCards [1].innerHTML){
-  openCards [0].classList.add("boxMatch");
-  openCards [1].classList.add("boxMatch");
-}else{
-  openCards [0].classList.remove("boxOpen");
-  openCards [1].classList.remove("boxOpen");
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
 }
-openCards= [ ]; if(document.querySelectorAll(".boxMatch").length === emojis.length){
-alert("Você venceu! ");
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+    
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu!");
+    }
 }
-}
+
